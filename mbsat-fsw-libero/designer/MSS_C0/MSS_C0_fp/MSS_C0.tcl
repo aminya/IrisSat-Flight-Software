823c148f9cf4be9e7cb4a3f2b115a91fd7191b17
Joseph Howarth <josephhowarth17@gmail.com>
2020-05-05 19:19:09 -0500

    SPI_0 Pins changed to be avaialble in FF mode.
    - During ISP, the FPGA fabric is put into flash freeze mode where it no
    longer functions. So only peripherals with direct connections can
    communicate, i.e. SPI_0. The IO configuration was changed to enable the
    SPI_0 pins during flash freeze mode.
    
    - The clock during flash freeze was set to 50 mhz instead of 1mhz.

--- mbsat-fsw-libero/designer/MSS_C0/MSS_C0_fp/MSS_C0.tcl
+++ mbsat-fsw-libero/designer/MSS_C0/MSS_C0_fp/MSS_C0.tcl
@@ -1,22 +1,8 @@
-open_project -project {C:\Users\Files\MBSAT\working\ManitobaSat-Flight-Software\mbsat-fsw-libero\designer\MSS_C0\MSS_C0_fp\MSS_C0.pro}\
-         -connect_programmers {FALSE}
-if { [catch {load_programming_data \
-    -name {M2S010} \
-    -fpga {C:\Users\Files\MBSAT\working\ManitobaSat-Flight-Software\mbsat-fsw-libero\designer\MSS_C0\MSS_C0.map} \
-    -header {C:\Users\Files\MBSAT\working\ManitobaSat-Flight-Software\mbsat-fsw-libero\designer\MSS_C0\MSS_C0.hdr} \
-    -envm {C:\Users\Files\MBSAT\working\ManitobaSat-Flight-Software\mbsat-fsw-libero\designer\MSS_C0\MSS_C0.efc}  \
-    -spm {C:\Users\Files\MBSAT\working\ManitobaSat-Flight-Software\mbsat-fsw-libero\designer\MSS_C0\MSS_C0.spm} \
-    -dca {C:\Users\Files\MBSAT\working\ManitobaSat-Flight-Software\mbsat-fsw-libero\designer\MSS_C0\MSS_C0.dca} } return_val] } {
-    save_project
-    close_project
-    exit }
-if { [catch {export_single_ppd \
-    -name {M2S010} \
-    -file {C:\Users\Files\MBSAT\working\ManitobaSat-Flight-Software\mbsat-fsw-libero\designer\MSS_C0\MSS_C0.ppd}} return_val ] } {
-    save_project
-    close_project
-    exit}
-
+open_project -project {C:\Users\Files\MBSAT\working\ManitobaSat-Flight-Software\mbsat-fsw-libero\designer\MSS_C0\MSS_C0_fp\MSS_C0.pro}
+enable_device -name {M2S010} -enable 1
+set_programming_file -name {M2S010} -file {C:\Users\Files\MBSAT\working\ManitobaSat-Flight-Software\mbsat-fsw-libero\designer\MSS_C0\MSS_C0.ppd}
+set_programming_action -action {PROGRAM} -name {M2S010} 
+run_selected_actions
 set_programming_file -name {M2S010} -no_file
 save_project
 close_project
